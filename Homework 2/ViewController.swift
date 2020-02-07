478966c389747bed2829d411a049050feaed6678
Elias Mobrin <l.mobrin@setonhill.edu>
2020-02-06 20:36:54 -0500

    Done

--- Homework 2/ViewController.swift
+++ Homework 2/ViewController.swift
@@ -9,6 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var name1: UITextField!
+    @IBOutlet weak var gender1: UITextField!
+    @IBOutlet weak var animal1: UITextField!
+    @IBOutlet weak var colour: UITextField!
+    @IBOutlet weak var animal2: UITextField!
+    @IBOutlet weak var name2: UITextField!
+    @IBOutlet weak var gender2: UITextField!
+    
+    @IBOutlet weak var MadLib: UITextView!
+    
+    
+    @IBAction func button(_ sender: UIButton) {
+        MadLib.text = name1.text! + "was walking in the park one day, human to " + gender1.text! + "self. The day was bright, the sun was shining, and the" + animal1.text! + " were singing. Suddenly, two" + colour.text! + animal2.text! + "ran by. Run! Run! they cried." + name2.text! + " is coming! And " + gender2.text! + "'s really angry!"
+        
+        
+        
+    }
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
